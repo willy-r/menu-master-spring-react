72b4743358cbd1bb39fd2bc0419ff483b58d496c
William Rodrigues <williamrodrigues2442@gmail.com>
2023-06-01 22:17:10 -0300

    feat: creates frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Manu <span>Master</span></h1>
+      <div className="card-grid">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  process.env = {...process.env, ...loadEnv(mode, process.cwd(), '')};
+  return {
+    // vite config
+    plugins: [react()],
+    server: {
+      port: parseInt(process.env.VITE_APP_PORT),
+    },
+  }
+});
